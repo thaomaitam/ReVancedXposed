71af3ee2db0f483586905913fbd1e966c9f58e35
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2025-06-12 15:27:32 +0800

    VideoAds: support YT `20.24.33`

--- app/src/main/java/io/github/chsbuffer/revancedxposed/youtube/ad/VideoAds.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/youtube/ad/VideoAds.kt
@@ -3,17 +3,12 @@ package io.github.chsbuffer.revancedxposed.youtube.ad
 import de.robv.android.xposed.XC_MethodReplacement
 import io.github.chsbuffer.revancedxposed.youtube.YoutubeHook
 
-
 fun YoutubeHook.VideoAds() {
     getDexMethod("LoadVideoAds") {
         findMethod {
             matcher {
                 usingEqStrings(
-                    listOf(
-                        "TriggerBundle doesn't have the required metadata specified by the trigger ",
-                        "Tried to enter slot with no assigned slotAdapter",
-                        "Trying to enter a slot when a slot of same type and physical position is already active. Its status: ",
-                    )
+                    "TriggerBundle doesn't have the required metadata specified by the trigger "
                 )
             }
         }.single()
